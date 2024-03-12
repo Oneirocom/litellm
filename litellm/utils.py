a3117f995ba78be90ec33d50e291d98c94cac018
Krrish Dholakia <krrishdholakia@gmail.com>
2024-03-12 10:55:54 -0700

    fix(azure.py): support cost tracking for azure/dall-e-3

--- litellm/utils.py
+++ litellm/utils.py
@@ -3842,7 +3842,9 @@ def completion_cost(
                     * n
                 )
             else:
-                raise Exception(f"Model={model} not found in completion cost model map")
+                raise Exception(
+                    f"Model={image_gen_model_name} not found in completion cost model map"
+                )
         # Calculate cost based on prompt_tokens, completion_tokens
         if (
             "togethercomputer" in model
